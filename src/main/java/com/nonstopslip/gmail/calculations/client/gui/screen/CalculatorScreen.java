a3f254270b46a6284a016731fb83510eede618f4
nonstopslip <nonstopslip@gmail.com>
2023-04-26 18:58:59 +0200

    Add files via upload
    Fixed key input behaviour

--- src/main/java/com/nonstopslip/gmail/calculations/client/gui/screen/CalculatorScreen.java
+++ src/main/java/com/nonstopslip/gmail/calculations/client/gui/screen/CalculatorScreen.java
@@ -1,18 +1,25 @@
 package com.nonstopslip.gmail.calculations.client.gui.screen;
 
 import com.nonstopslip.gmail.calculations.client.gui.Terminal;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.GridWidget;
 import net.minecraft.client.gui.widget.SimplePositioningWidget;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 
+@Environment(EnvType.CLIENT)
 public class CalculatorScreen extends Screen {
 
     private static CalculatorScreen instance;
+    private boolean keyHeld = false;
     private Terminal terminal;
 
     private CalculatorScreen(Text title) {
@@ -27,6 +34,17 @@ public class CalculatorScreen extends Screen {
         return instance;
     }
 
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+
 
     @Override
     public void init() {
@@ -39,9 +57,27 @@ public class CalculatorScreen extends Screen {
         gridWidget.getMainPositioner().margin(0, 1, 1, 0);
         GridWidget.Adder adder = gridWidget.createAdder(5);
         adder.add(this.terminal, 5);
-        for (String s : BUTTON_SYMBOLS) {
-            adder.add(this.createButton(s));
-        }
+        adder.add(createNumberButton(7));
+        adder.add(createNumberButton(8));
+        adder.add(createNumberButton(9));
+        adder.add(createButton("DEL", button -> this.terminal.deleteLast()));
+        adder.add(createButton("C", button -> this.terminal.clear()));
+        adder.add(createNumberButton(4));
+        adder.add(createNumberButton(5));
+        adder.add(createNumberButton(6));
+        adder.add(createButton("+", button -> this.terminal.setOperation(Terminal.ADD)));
+        adder.add(createButton("-", button -> this.terminal.setOperation(Terminal.SUBTRACT)));
+        adder.add(createNumberButton(1));
+        adder.add(createNumberButton(2));
+        adder.add(createNumberButton(3));
+        adder.add(createButton("×", button -> this.terminal.setOperation(Terminal.MULTIPLY)));
+        adder.add(createButton("÷", button -> this.terminal.setOperation(Terminal.DIVIDE)));
+        adder.add(createButton("MOD", button -> this.terminal.setOperation(Terminal.MODULUS)));
+        adder.add(createButton("+|-", button -> this.terminal.negate()));
+        adder.add(createNumberButton(0));
+        adder.add(createButton(",", button -> this.terminal.insertPeriod()));
+        adder.add(createButton("=", button -> this.terminal.evaluate()));
+
         gridWidget.refreshPositions();
         SimplePositioningWidget.setPos(gridWidget, 0, 0, this.width, this.height, 0.5f, 0.5f);
         gridWidget.forEachChild(this::addDrawableChild);
@@ -49,100 +85,68 @@ public class CalculatorScreen extends Screen {
 
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-    }
-
-    @Override
-    public boolean shouldPause() {
-        return false;
-    }
-
-    public void buttonPressed(int id) {
-        if (id >= "0".hashCode() && id <= "9".hashCode())
-            this.terminal.insertDigit(id - "0".hashCode());
-
-        else if (id == ADD_ID)
-            this.terminal.setOperation(Terminal.ADD);
-        else if (id == SUBTRACT_ID)
-            this.terminal.setOperation(Terminal.SUBTRACT);
-        else if (id == MULTIPLY_ID)
-            this.terminal.setOperation(Terminal.MULTIPLY);
-        else if (id == DIVIDE_ID)
-            this.terminal.setOperation(Terminal.DIVIDE);
-        else if (id == MODULUS_ID)
-            this.terminal.setOperation(Terminal.MODULUS);
-
-        else if (id == DELETE_ID)
-            this.terminal.deleteLast();
-        else if (id == CLEAR_ID)
-            this.terminal.clear();
-        else if (id == NEGATE_ID)
-            this.terminal.negate();
-        else if (id == PERIOD_ID)
-            this.terminal.insertPeriod();
-        else if (id == EQUALS_ID)
-            this.terminal.evaluate();
-    }
-
-
-    @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (keyCode >= GLFW.GLFW_KEY_0 && keyCode <= GLFW.GLFW_KEY_9)
-            this.terminal.insertDigit(keyCode - GLFW.GLFW_KEY_0);
-        else if (keyCode >= GLFW.GLFW_KEY_KP_0 && keyCode <= GLFW.GLFW_KEY_KP_9)
-            this.terminal.insertDigit(keyCode - GLFW.GLFW_KEY_KP_0);
+        if (this.keyHeld)
+            return false;
 
-        else if (keyCode == GLFW.GLFW_KEY_KP_ADD)
-            this.terminal.setOperation(Terminal.ADD);
-        else if (keyCode == GLFW.GLFW_KEY_KP_SUBTRACT)
-            this.terminal.setOperation(Terminal.SUBTRACT);
-        else if (keyCode == GLFW.GLFW_KEY_KP_MULTIPLY)
-            this.terminal.setOperation(Terminal.MULTIPLY);
-        else if (keyCode == GLFW.GLFW_KEY_SLASH || keyCode == GLFW.GLFW_KEY_KP_DIVIDE)
-            this.terminal.setOperation(Terminal.DIVIDE);
-        else if (keyCode == GLFW.GLFW_KEY_M)
-            this.terminal.setOperation(Terminal.MODULUS);
+        System.out.println(keyCode);
+        this.keyHeld = true;
 
-        else if (keyCode == GLFW.GLFW_KEY_MINUS)
-            this.terminal.negate();
-        else if (keyCode == GLFW.GLFW_KEY_PERIOD || keyCode == GLFW.GLFW_KEY_COMMA)
-            this.terminal.insertPeriod();
-        else if (keyCode == GLFW.GLFW_KEY_ENTER || keyCode == GLFW.GLFW_KEY_KP_ENTER)
-            this.terminal.evaluate();
-        else if (keyCode == GLFW.GLFW_KEY_BACKSPACE)
-            this.terminal.deleteLast();
-        else if (keyCode == GLFW.GLFW_KEY_DELETE)
-            this.terminal.clear();
+        switch (keyCode) {
+            case GLFW.GLFW_KEY_0, GLFW.GLFW_KEY_1, GLFW.GLFW_KEY_2, GLFW.GLFW_KEY_3, GLFW.GLFW_KEY_4, GLFW.GLFW_KEY_5, GLFW.GLFW_KEY_6, GLFW.GLFW_KEY_7, GLFW.GLFW_KEY_8, GLFW.GLFW_KEY_9 ->
+                    this.terminal.insertDigit(keyCode - GLFW.GLFW_KEY_0);
+            case GLFW.GLFW_KEY_KP_0, GLFW.GLFW_KEY_KP_1, GLFW.GLFW_KEY_KP_2, GLFW.GLFW_KEY_KP_3, GLFW.GLFW_KEY_KP_4, GLFW.GLFW_KEY_KP_5, GLFW.GLFW_KEY_KP_6, GLFW.GLFW_KEY_KP_7, GLFW.GLFW_KEY_KP_8, GLFW.GLFW_KEY_KP_9 ->
+                    this.terminal.insertDigit(keyCode - GLFW.GLFW_KEY_KP_0);
 
-        return super.keyPressed(keyCode, scanCode, modifiers);
+            case GLFW.GLFW_KEY_KP_ADD ->
+                    this.terminal.setOperation(Terminal.ADD);
+            case GLFW.GLFW_KEY_KP_SUBTRACT ->
+                    this.terminal.setOperation(Terminal.SUBTRACT);
+            case GLFW.GLFW_KEY_KP_MULTIPLY ->
+                    this.terminal.setOperation(Terminal.MULTIPLY);
+            case GLFW.GLFW_KEY_KP_DIVIDE ->
+                    this.terminal.setOperation(Terminal.DIVIDE);
+            case GLFW.GLFW_KEY_M ->
+                    this.terminal.setOperation(Terminal.MODULUS);
+
+            case GLFW.GLFW_KEY_MINUS ->
+                    this.terminal.negate();
+            case GLFW.GLFW_KEY_PERIOD, GLFW.GLFW_KEY_KP_DECIMAL ->
+                    this.terminal.insertPeriod();
+            case GLFW.GLFW_KEY_BACKSPACE ->
+                    this.terminal.deleteLast();
+            case GLFW.GLFW_KEY_DELETE ->
+                    this.terminal.clear();
+            case GLFW.GLFW_KEY_ENTER, GLFW.GLFW_KEY_KP_ENTER ->
+                    this.terminal.evaluate();
+            default -> {
+                return super.keyPressed(keyCode, scanCode, modifiers);
+            }
+        }
+        MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+        return true;
     }
 
-    private static final String[] BUTTON_SYMBOLS = {
-            "7", "8", "9", "DEL", "C",
-            "4", "5", "6", "×", "÷",
-            "1", "2", "3", "+", "-",
-            "+|-", "0", ",", "MOD", "="
-    };
-    
-    private static final int DELETE_ID = BUTTON_SYMBOLS[3].hashCode();
-    private static final int CLEAR_ID = BUTTON_SYMBOLS[4].hashCode();
-    private static final int MULTIPLY_ID = BUTTON_SYMBOLS[8].hashCode();
-    private static final int DIVIDE_ID = BUTTON_SYMBOLS[9].hashCode();
-    private static final int ADD_ID = BUTTON_SYMBOLS[13].hashCode();
-    private static final int SUBTRACT_ID = BUTTON_SYMBOLS[14].hashCode();
-    private static final int NEGATE_ID = BUTTON_SYMBOLS[15].hashCode();
-    private static final int PERIOD_ID = BUTTON_SYMBOLS[17].hashCode();
-    private static final int MODULUS_ID = BUTTON_SYMBOLS[18].hashCode();
-    private static final int EQUALS_ID = BUTTON_SYMBOLS[19].hashCode();
-    
-    
-    private ButtonWidget createButton(String s) {
-        return ButtonWidget.builder(Text.of(s), (button) -> {
+
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        this.keyHeld = false;
+        return super.keyReleased(keyCode, scanCode, modifiers);
+    }
+
+    private static ButtonWidget createButton(String s, ButtonWidget.PressAction pressAction) {
+        return ButtonWidget.builder(Text.of(s), button -> {
+            pressAction.onPress(button);
             button.setFocused(false);
-            this.buttonPressed(button.getMessage().getString().hashCode());
         }).size(40, 40).build();
+    }
+
+    private static ButtonWidget createNumberButton(int n) {
+        return createButton(String.valueOf(n), (button) -> {
+            if (MinecraftClient.getInstance().currentScreen != null && MinecraftClient.getInstance().currentScreen instanceof CalculatorScreen) {
+                ((CalculatorScreen) MinecraftClient.getInstance().currentScreen).terminal.insertDigit(n);
+            }
+        });
     }
 
 }
